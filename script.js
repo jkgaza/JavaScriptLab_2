59173ff712646be76f0765b33b35df38d7119b75
Jordan Gaza <Gaza>
2019-11-08 15:49:46 -0500

    I think I got the random health counter?? I have no idea

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+/** @todo Figure this out and don't die in the process */
+
 let grantHealth = "";
 let userHealth = "";
 let grantDeath = "";
 
+//initial prompt
+
 let answer = prompt("Would you like to play a game?");
 
-if (answer === "yes") {
+if (answer === "yes","Yes") {
   document.getElementById("start").innerHTML =
   console.log(prompt("Great! What is your name?"));
 }
 
 
-while(userHealth>0 & grantDeaths<3){
-
+//while loop
+while (userHealth > 0 & grantDeath < 3){
+    grantHealth == Math.floor(Math.random() * 3);
+    userHealth == Math.floor(Math.random() * 3);
 }
+//haha i dont know what im doing
+// function playerName(name){
+//     const answer = getRandomAnswer(answers);
+    
+//     generateResponse(name, answer);
+
+// }
+
+
+// function generateResponse(name, answer){
+//     console.log(`You asked: ${name}`);
+//     console.log(`Our answer is: ${answer}`);
+
+// }
 
 
 /**An if statement?? If user says yes to playing game, leads to
