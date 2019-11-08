2df7277695bcf3665db0611ae81fcfb9e88f3161
Jordan Gaza <Gaza>
2019-11-08 12:22:16 -0500

    Figured out nesting prompts? Now need to figure out how to assign second prompt output to user

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 let grantHealth = "";
 let userHealth = "";
 let grantDeath = "";
+
+let answer = prompt("Would you like to play a game?");
+
+if (answer === "yes") {
+  document.getElementById("start").innerHTML =
+  console.log(prompt("Great! What is your name?"));
+}
+
+
+while(userHealth>0 & grantDeaths<3){
+
+}
+
+
+/**An if statement?? If user says yes to playing game, leads to
+ * another prompt where name is asked? If no, something like "ur lame". 
+ * How to initialize actual game after that
+ */
