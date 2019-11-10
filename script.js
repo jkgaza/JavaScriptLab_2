09c399de4c1247b4fc4d212b26b0c469134c27b1
Jordan Gaza <Gaza>
2019-11-10 14:50:11 -0500

    I think I got it. Maybe.

--- script.js
+++ script.js
@@ -8,36 +8,37 @@ let grantDeath = "";
 
 let answer = prompt("Would you like to play a game?");
 
-if ((answer === "yes", "Yes")) {
-  //   document.getElementById("start").innerHTML =
-  let user = prompt("Great! What is your name?");
-}
-
 let user = "";
 
-//while loop
-while ((userHealth > 0, grantDeath < 3)) {
-  grantHealth == grantHealth --- Math.floor(Math.random() * 3);
-  userHealth == userHealth --- Math.floor(Math.random() * 3);
-  console.log(user + " has " + userHealth + " left.");
-  console.log("Grant the Great has " + grantHealth + " left.");
-  break
+if ((answer === "yes" , "Yes")) {
+  user = prompt("Great! What is your name?");
 }
 
 
-//haha i dont know what im doing
-// function playerName(name){
-//     const answer = getRandomAnswer(answers);
+//while loop
+while (userHealth > 0 && grantDeath < 3) {
+  grantHealth = grantHealth - Math.floor(Math.random() * 3);
+  userHealth = userHealth - Math.floor(Math.random() * 3);
+  console.log(user + " has " + userHealth + " left.");
+  console.log("Grant the Great has " + grantHealth + " left.");
 
-//     generateResponse(name, answer);
+  if (grantHealth <= 0) {
+    grantHealth = 10;
+    grantDeath++;
+    console.log("Grant died!");
+//   } else if (userHealth <= 0) {
+  //    console.log("Grant the Great is the winner");
+  }
 
-// }
+}
 
-// function generateResponse(name, answer){
-//     console.log(`You asked: ${name}`);
-//     console.log(`Our answer is: ${answer}`);
+if (grantDeath = 3){
+    console.log(user + " has won!")
 
-// }
+}
+
+
+
 
 /**An if statement?? If user says yes to playing game, leads to
  * another prompt where name is asked? If no, something like "ur lame".
